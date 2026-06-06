fc448031e9323cf9f7490fef0ac8a3ade3a9ae8f
hawk <nahuel36gg@gmail.com>
2026-06-06 19:24:58 -0300

    agregada sublista al inspector de UI Toolkit

--- Assets/UI Toolkit/Example/UIToolkitEnInspector.cs
+++ Assets/UI Toolkit/Example/UIToolkitEnInspector.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 public class UIToolkitEnInspector : MonoBehaviour
 {
     public string titulo = "Hello World!";
@@ -13,4 +14,11 @@ public class MiListaEnInspector
 {
     public string nombre;
     public int valor;
+    public List<MiListaDentroDeListaEnInspector> miListaDentroDeLista = new List<MiListaDentroDeListaEnInspector>();
+}
+
+[System.Serializable]
+public class  MiListaDentroDeListaEnInspector
+{
+    public string subNombre;
 }
